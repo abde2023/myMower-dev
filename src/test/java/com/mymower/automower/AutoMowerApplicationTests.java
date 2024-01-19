e297c7f99018c223ee1857f6c3c73f55af795a63
abderrahmen ben jemaa <abderrahmen.ben-jemaa@vo2-group.com>
2024-01-19 14:27:04 +0100

    initial springboot application

---
+++ src/test/java/com/mymower/automower/AutoMowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymower.automower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
